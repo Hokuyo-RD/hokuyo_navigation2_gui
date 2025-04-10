9ef9d78bf37e2339ed3cc606185403da3a37e030
s-takahashijp <shallow.e1621@gmail.com>
2025-04-10 17:15:06 +0900

    04/10 階層分け

--- wada/rest_server_sample/test_server.py
+++ wada/rest_server_sample/test_server.py
@@ -1,48 +1,48 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import subprocess
 
 app = Flask(__name__)
 
-@app.route('/trigger', methods=['GET', 'POST'])
+@app.route('/indoor_run')
+def indoor_run():
+    return render_template('indoor_run.html')
+
+@app.route('/outdoor_run')
+def outdoor_run():
+    return render_template('outdoor_run.html')
+
+@app.route('/stop')
+def stop_run():
+    return render_template('stop.html')
+
+@app.route('/tools')
+def tools_run():
+    return render_template('toools.html')
+
+@app.route('/', methods=['GET', 'POST'])
 def trigger_script():
     if request.method == 'GET':
-        return '''
-            <form method="post" action="/trigger">
-                <button type="submit" name="command" value="run" style="width:200px;height:100px">自律移動開始</button>
-            </form>
-            <form method="post" action="/trigger">
-                <button type="submit" name="command" value="stop" style="width:200px;height:100px">プログラム停止</button>
-            </form>
-            <form method="post" action="/trigger">
-                <button type="submit" name="command" value="tools" style="width:200px;height:100px">設定ツール起動</button>
-            </form>
-            <a href="http://192.168.137.28:8085/ros_map_demo/">マップ</a>
-            '''
+        return render_template('index.html')
+    
     elif request.method == 'POST':
         command = request.form.get("command") or request.get_json().get("command")
-        if command == "run":
-            subprocess.run(["/home/hokuyo/catkin_ws/src/expo_wizurg/scripts/expo_nav.sh"])
-            return '''
-                    <h1>WizURG操作画面</h1>
-                    <p>自律移動プログラムが実行されました。</p>
-                '''
+        if command == "indoor_run":
+            subprocess.run(["/home/hokuyo/catkin_ws/src/expo_wizurg/scripts/expo_in.sh"])
+            return render_template('indoor_run.html')
+        elif command == "outdoor_run":
+            subprocess.run(["/home/hokuyo/catkin_ws/src/expo_wizurg/scripts/expo_out.sh"])
+            return render_template('outdoor_run.html')
         elif command == "stop":
             subprocess.run(["/home/hokuyo/catkin_ws/src/expo_wizurg/scripts/web_kill_all_rosnode.sh"])
-            return '''
-                    <h1>WizURG操作画面</h1>
-                    <p>プログラムを終了します。</p>
-                '''
+            return render_template('stop.html')
         elif command == "tools":
             subprocess.run(["/home/hokuyo/catkin_ws/src/expo_wizurg/scripts/expo_start.sh"])
-            return '''
-                    <h1>WizURG操作画面</h1>
-                    <p>設定ツールを起動します。</p>
-                '''
+            return render_template('tools.html')
         else:
             print(command)
             return "Unknown command", 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5050)

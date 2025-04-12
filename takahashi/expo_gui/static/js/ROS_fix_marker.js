96b6e5a9f3ee88cc2abc87a5c464a7095be7198a
s-takahashijp <shallow.e1621@gmail.com>
2025-04-12 20:23:54 +0900

    04/12 動作確認

--- takahashi/expo_gui/static/js/ROS_fix_marker.js
+++ takahashi/expo_gui/static/js/ROS_fix_marker.js
@@ -158,8 +158,9 @@ window.onload = (event) => {
   let ros = new ROSLIB.Ros({
     //url : 'ws://192.168.137.31:9090'
     //url : 'ws://192.168.0.157:9090'
-    //url : 'ws://localhost:9090'
-    url: 'ws://' + location.hostname + ':9090'
+    url : 'wss://takahashi-thinkpad-x13-gen-2.tailfa031c.ts.net/ws'
+    //url: 'ws://' + location.hostname + ':9090'
+    //url: 'ws://192.168.0.8:9090'
   });
 
   ros.on('connection', function () {
